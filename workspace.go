798a8ce862d285e9888710b0834d28b416bf7c9e
shaghayeghFth <shaghayegh@cloudsquid.io>
2025-05-05 12:50:30 +0330

    add workspace methods

---
+++ workspace.go
@@ -0,0 +1,175 @@
+package pipedream
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"path"
+)
+
+type GetWorkspaceResponse struct {
+	Data Workspace `json:"data"`
+}
+
+type Workspace struct {
+	ID                string `json:"id"`
+	OrgName           string `json:"orgname"`
+	Name              string `json:"name"`
+	Email             string `json:"email"`
+	DailyCreditsQuota int    `json:"daily_credits_quota"`
+	DailyCreditsUsed  int    `json:"daily_credits_used"`
+}
+
+type GetWorkspaceConnectedAccountsResponse struct {
+	PageInfo PageInfo           `json:"page_info"`
+	Data     []ConnectedAccount `json:"data"`
+}
+
+type ConnectedAccount struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+type GetWorkspaceSubscriptionsResponse struct {
+	Data []Subscription `json:"data"`
+}
+
+type Subscription struct {
+	ID         string `json:"id"`
+	EmitterID  string `json:"emitter_id"`
+	ListenerID string `json:"listener_id"`
+	EventID    string `json:"event_id"`
+}
+
+// GetWorkspace views your workspace’s current credit usage for the billing period in real time
+func (c *Client) GetWorkspace(
+	ctx context.Context,
+	orgID string,
+) (*GetWorkspaceResponse, error) {
+	c.logger.Debug("get workspace")
+
+	if orgID == "" {
+		return nil, fmt.Errorf("orgID is required")
+	}
+
+	baseURL := c.baseURL.ResolveReference(&url.URL{
+		Path: path.Join(c.baseURL.Path, "workspaces", orgID),
+	})
+
+	endpoint := baseURL.String()
+
+	req, err := http.NewRequest(http.MethodGet, endpoint, nil)
+	if err != nil {
+		return nil, fmt.Errorf("creating get workspace request: %w", err)
+	}
+
+	response, err := c.doRequestViaApiKey(ctx, req)
+	if err != nil {
+		return nil, fmt.Errorf("executing get workspace request: %w", err)
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode < 200 || response.StatusCode >= 300 {
+		raw, _ := io.ReadAll(response.Body)
+		return nil, fmt.Errorf("unexpected status code %d: %s", response.StatusCode, string(raw))
+	}
+
+	var result GetWorkspaceResponse
+	if err := unmarshalResponse(response, &result); err != nil {
+		return nil, fmt.Errorf("unmarshalling get workflow details response:e: %w", err)
+	}
+
+	return &result, nil
+}
+
+// GetWorkspaceConnectedAccounts Retrieves all the connected accounts for a specific workspace
+func (c *Client) GetWorkspaceConnectedAccounts(
+	ctx context.Context,
+	orgID string,
+	query string, // optional
+) (*GetWorkspaceConnectedAccountsResponse, error) {
+	c.logger.Debug("get workspace connected accounts")
+
+	if orgID == "" {
+		return nil, fmt.Errorf("orgID is required")
+	}
+
+	baseURL := c.baseURL.ResolveReference(&url.URL{
+		Path: path.Join(c.baseURL.Path, "workspaces", orgID, "accounts"),
+	})
+
+	queryParams := url.Values{}
+
+	if query != "" {
+		addQueryParams(queryParams, "query", query)
+	}
+	baseURL.RawQuery = queryParams.Encode()
+
+	endpoint := baseURL.String()
+
+	req, err := http.NewRequest(http.MethodGet, endpoint, nil)
+	if err != nil {
+		return nil, fmt.Errorf("creating get workspace accounts request: %w", err)
+	}
+
+	response, err := c.doRequestViaApiKey(ctx, req)
+	if err != nil {
+		return nil, fmt.Errorf("executing get workspace accounts request: %w", err)
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode < 200 || response.StatusCode >= 300 {
+		raw, _ := io.ReadAll(response.Body)
+		return nil, fmt.Errorf("unexpected status code %d: %s", response.StatusCode, string(raw))
+	}
+
+	var result GetWorkspaceConnectedAccountsResponse
+	if err := unmarshalResponse(response, &result); err != nil {
+		return nil, fmt.Errorf("unmarshalling get workflow accounts response:e: %w", err)
+	}
+
+	return &result, nil
+}
+
+// GetWorkspaceSubscriptions Retrieves all the subscriptions configured for a specific workspace
+func (c *Client) GetWorkspaceSubscriptions(
+	ctx context.Context,
+	orgID string,
+) (*GetWorkspaceSubscriptionsResponse, error) {
+	c.logger.Debug("get workspace subcriptions")
+
+	if orgID == "" {
+		return nil, fmt.Errorf("orgID is required")
+	}
+
+	baseURL := c.baseURL.ResolveReference(&url.URL{
+		Path: path.Join(c.baseURL.Path, "workspaces", orgID, "subscriptions"),
+	})
+
+	endpoint := baseURL.String()
+
+	req, err := http.NewRequest(http.MethodGet, endpoint, nil)
+	if err != nil {
+		return nil, fmt.Errorf("creating get workspace subscriptions request: %w", err)
+	}
+
+	response, err := c.doRequestViaApiKey(ctx, req)
+	if err != nil {
+		return nil, fmt.Errorf("executing get workspace subscriptions request: %w", err)
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode < 200 || response.StatusCode >= 300 {
+		raw, _ := io.ReadAll(response.Body)
+		return nil, fmt.Errorf("unexpected status code %d: %s", response.StatusCode, string(raw))
+	}
+
+	var result GetWorkspaceSubscriptionsResponse
+	if err := unmarshalResponse(response, &result); err != nil {
+		return nil, fmt.Errorf("unmarshalling get workflow subscriptions response:e: %w", err)
+	}
+
+	return &result, nil
+}
---
+++ workspace_test.go
@@ -0,0 +1,170 @@
+package pipedream
+
+import (
+	"context"
+	"fmt"
+	"github.com/stretchr/testify/suite"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+	"time"
+)
+
+type workspacesTestSuite struct {
+	suite.Suite
+	ctx             context.Context
+	pipedreamClient *Client
+}
+
+func (suite *workspacesTestSuite) SetupTest() {
+	suite.ctx = context.Background()
+	suite.pipedreamClient = &Client{
+		projectID:   "project-abc",
+		environment: "development",
+		token: &Token{
+			AccessToken: "dummy-token",
+			TokenType:   "Bearer",
+			ExpiresIn:   3600,
+			CreatedAt:   int(time.Now().Unix()),
+			ExpiresAt:   time.Now().Add(1 * time.Hour),
+		},
+		logger: &mockLogger{},
+		apiKey: "dummy-key",
+	}
+}
+
+func (suite *workspacesTestSuite) TestGetWorkspaces_Success() {
+	require := suite.Require()
+	orgID := "o_Qa8I1Z"
+	expectedResponse := `{
+		"data": {
+			"id": "o_Qa8I1Z",
+			"orgname": "asdf",
+			"name": "asdf",
+			"email": "dev@pipedream.com",
+			"daily_credits_quota": 100,
+			"daily_credits_used": 0
+		}
+	}`
+
+	expectedPath := "/workspaces/o_Qa8I1Z"
+
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		require.Equal(expectedPath, r.URL.Path)
+		require.Equal(http.MethodGet, r.Method)
+
+		w.WriteHeader(http.StatusOK)
+		_, _ = fmt.Fprint(w, expectedResponse)
+	}))
+	defer server.Close()
+
+	restParsed, err := url.Parse(server.URL)
+	require.NoError(err)
+
+	suite.pipedreamClient.httpClient = server.Client()
+	suite.pipedreamClient.baseURL = restParsed
+
+	resp, err := suite.pipedreamClient.GetWorkspace(
+		context.Background(),
+		orgID,
+	)
+	require.NoError(err)
+	require.NotNil(resp)
+	require.Equal("o_Qa8I1Z", resp.Data.ID)
+}
+
+func (suite *workspacesTestSuite) TestGetWorkspaceConnectedAccounts_Success() {
+	require := suite.Require()
+	orgID := "o_Qa8I1Z"
+	expectedResponse := `{
+	  "page_info": {
+		"total_count": 1,
+		"count": 1,
+		"start_cursor": "YXBuXzJrVmhMUg",
+		"end_cursor": "YXBuXzJrVmhMUg"
+	  },
+	  "data": [
+		{
+		  "id": "apn_2kVhLR",
+		  "name": "Google Sheets #1"
+		}
+	  ]
+	}`
+
+	expectedPath := "/workspaces/o_Qa8I1Z/accounts"
+
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		require.Equal(expectedPath, r.URL.Path)
+		require.Equal(http.MethodGet, r.Method)
+
+		w.WriteHeader(http.StatusOK)
+		_, _ = fmt.Fprint(w, expectedResponse)
+	}))
+	defer server.Close()
+
+	restParsed, err := url.Parse(server.URL)
+	require.NoError(err)
+
+	suite.pipedreamClient.httpClient = server.Client()
+	suite.pipedreamClient.baseURL = restParsed
+
+	resp, err := suite.pipedreamClient.GetWorkspaceConnectedAccounts(
+		context.Background(),
+		orgID,
+		"",
+	)
+	require.NoError(err)
+	require.NotNil(resp)
+	require.Equal("apn_2kVhLR", resp.Data[0].ID)
+}
+
+func (suite *workspacesTestSuite) TestGetWorkspaceSubscriptions_Success() {
+	require := suite.Require()
+	orgID := "o_Qa8I1Z"
+	expectedResponse := `{
+	  "data": [
+		{
+		  "id": "sub_abc123",
+		  "emitter_id": "dc_abc123",
+		  "listener_id": "p_abc123",
+		  "event_name": ""
+		},
+		{
+		  "id": "sub_def456",
+		  "emitter_id": "dc_def456",
+		  "listener_id": "p_def456",
+		  "event_name": ""
+		}
+	  ]
+	}`
+
+	expectedPath := "/workspaces/o_Qa8I1Z/subscriptions"
+
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		require.Equal(expectedPath, r.URL.Path)
+		require.Equal(http.MethodGet, r.Method)
+
+		w.WriteHeader(http.StatusOK)
+		_, _ = fmt.Fprint(w, expectedResponse)
+	}))
+	defer server.Close()
+
+	restParsed, err := url.Parse(server.URL)
+	require.NoError(err)
+
+	suite.pipedreamClient.httpClient = server.Client()
+	suite.pipedreamClient.baseURL = restParsed
+
+	resp, err := suite.pipedreamClient.GetWorkspaceSubscriptions(
+		context.Background(),
+		orgID,
+	)
+	require.NoError(err)
+	require.NotNil(resp)
+	require.Equal("sub_def456", resp.Data[1].ID)
+}
+
+func TestWorkspaces(t *testing.T) {
+	suite.Run(t, new(workspacesTestSuite))
+}
